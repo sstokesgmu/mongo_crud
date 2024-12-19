0dcc144c6e0a7737f4df2a962903c7bd5ce405ab
sstokes <sterlingjstokes@gmail.com>
2024-12-19 09:43:15 -0500

    Revert "quick push"
    This reverts commit 5b0841b0dea27149d608023586a9bb7de79b71a2.

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 
-//? How does this import work
-const connectionToDatabase = require("./db/conn");
-connectionToDatabase();
 //Gets the env port to index.js or uses a backup port
 const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 })
-
